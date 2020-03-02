f3cb4e91ed8ba440e466598154bd348133524016
Jon Ander Perez <jonanproject@gmail.com>
2020-03-02 20:43:36 +0100

    Fin de seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Equipo } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,27 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: Equipo[];
   cargada = false;
 
   constructor(private http: HttpClient) {
-    //Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe((resp: InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
-      console.log(resp);
-    });
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+   private cargarInfo(){
+      //Leer el archivo JSON
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+   }
+
+   private cargarEquipo(){
+      this.http.get('https://angular-html-ddbb2.firebaseio.com/equipo.json')
+      .subscribe((resp: Equipo[]) => {
+        this.equipo = resp;
+      });
    }
 }
